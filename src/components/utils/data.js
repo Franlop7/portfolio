3c805b9ca4b7f8376783f9e8899b5e4e376b93f6
Fran Lopez <franciscolopto@gmail.com>
2023-04-10 18:43:11 +0200

    add utils data projects

---
+++ src/components/utils/data.js
@@ -0,0 +1,30 @@
+export const data = [
+  {
+    title: 'Codepaster',
+    body: 'Es una web educativa, para compartir codigo. El profesor puede mandar codigo, modificarlo etc.. y el alumno ve los resultados a tiempo real.',
+    img: '/img/codepaster.webp',
+    deploy: 'https://codepaster.net/',
+    link: 'https://github.com/Franlop7/code-paster/'
+  },
+  {
+    title: 'Codepaster',
+    body: 'Es una web educativa, para compartir codigo. El profesor puede mandar codigo, modificarlo etc.. y el alumno ve los resultados a tiempo real.',
+    img: '/img/codepaster.webp',
+    deploy: 'https://codepaster.net/',
+    link: 'https://github.com/Franlop7/code-paster/'
+  },
+  {
+    title: 'Codepaster',
+    body: 'Es una web educativa, para compartir codigo. El profesor puede mandar codigo, modificarlo etc.. y el alumno ve los resultados a tiempo real.',
+    img: '/img/codepaster.webp',
+    deploy: 'https://codepaster.net/',
+    link: 'https://github.com/Franlop7/code-paster/'
+  },
+  {
+    title: 'Codepaster',
+    body: 'Es una web educativa, para compartir codigo. El profesor puede mandar codigo, modificarlo etc.. y el alumno ve los resultados a tiempo real.',
+    img: '/img/codepaster.webp',
+    deploy: 'https://codepaster.net/',
+    link: 'https://github.com/Franlop7/code-paster/'
+  }
+]
