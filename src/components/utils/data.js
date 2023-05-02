7b116c6ea3181a0188e44ceeca644ea134de4ee1
Fran Lopez <franciscolopto@gmail.com>
2023-05-02 20:15:17 +0200

    change language

--- src/components/utils/data.js
+++ src/components/utils/data.js
@@ -1,7 +1,7 @@
 export const data = [
   {
     title: 'Codepaster',
-    body: 'Es una web educativa, para compartir codigo. El profesor puede mandar codigo, modificarlo etc.. y el alumno ve los resultados a tiempo real.',
+    body: 'It is an educational website, to share code. The teacher can send code, modify it. and the student sees the results in real time.',
     img: '/img/codepaster.webp',
     deploy: 'https://codepaster.net/',
     link: 'https://github.com/Franlop7/code-paster/'
