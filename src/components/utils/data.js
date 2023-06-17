ed620b069886a40e01ac345ef21f436c72b8a47b
Fran pc bichin <franciscolopto@gmail.com>
2023-06-17 10:44:01 +0200

    add new project

--- src/components/utils/data.js
+++ src/components/utils/data.js
@@ -14,11 +14,11 @@ export const data = [
     link: 'https://github.com/Franlop7/Editor-y-Preview-Markdown/'
   },
   {
-    title: 'New project coming soon',
-    body: 'The new projects are more than interesting. We just have to wait a bit longer. If you have any suggestions, dont hesitate to send me an email or contact me through any of my social networks. Thank.',
-    img: '/img/comingsoon.webp',
-    deploy: 'https://franlopez.pages.dev/',
-    link: 'https://github.com/Franlop7/'
+    title: 'How to deploy your websites and app',
+    body: 'It is a laboratory explaining how to deploy a web or app in the cloud, with different providers. Both free and paid, easily and step by step.',
+    img: '/img/cloud.webp',
+    deploy: 'https://github.com/Franlop7/Master-Front-End/tree/main/modulo7-cloud',
+    link: 'https://github.com/Franlop7/Master-Front-End/tree/main/modulo7-cloud'
   },
   {
     title: 'New project coming soon',
